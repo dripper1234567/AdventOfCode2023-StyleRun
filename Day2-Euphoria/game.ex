aeb7b323feb3d096624b26b4f1f3dfbe1e22e1e8
user <dripper1234567@gmail.com>
2023-12-03 00:56:48 +0800

    One step forward...

--- Day2-Euphoria/game.ex
+++ Day2-Euphoria/game.ex
@@ -1,6 +1,8 @@
 -- https://www.tutorialspoint.com/euphoria/euphoria_files_io.htm
 -- SO HELPFULL
 include std/sequence.e
+include std/convert.e
+include std/io.e
 
 -- SECTION: File Interp
 integer gameNum
@@ -23,47 +25,60 @@ constant BLUE = 14
 
 -- Sum of possible games
 integer TOTAL = 0
--- current game
-integer COUNT = 1
 
-line = gets(gameNum)
+sequence lines = read_lines(gameNum)
+sequence data
 
-printf(STDOUT, "PRINT STARTS HERE")
-loop do
-    printf(STDOUT, "LOOOOOOOOOOOP")
-    sequence nocolon = split(line, ": ")
-    if length(nocolon) > 0 then
-        nocolon = nocolon[2]
-        nocolon = split(nocolon, ", ")
-        printf( STDOUT, "Total Real Games : %s\n", {nocolon[1]})
-        -- FOR THE LENGTH OF SPLIT ON SEMICOLON
-            -- ISTRUE = TRUE
-            -- FOR THE LENGTH OF SPLIT ON COMMA
-                -- SPLIT ON SPACE
+for u = 1 to length(lines) do
+    data = split(lines[u], ": ")
 
-                -- IF 2 = red AND 1 > RED
-                    -- ISTRUE = FALSE
-                    -- EXIT
+    printf( STDOUT, "%s\n", {data[2]})
+    if length(data) > 0 then
+        -- Remove the game tag
+        data = data[2]
+        -- Split the games
+        data = split(data, "; ")
 
-                -- ELIF 2 = blue AND 1 > BLUE
-                    -- ISTRUE = FALSE
-                    -- EXIT
+        -- Check if the id is to be summed
+        integer ISTRUE = 1
 
-                -- ELIF 2 = green AND 1 > GREEN
-                    -- ISTRUE = FALSE
-                    -- EXIT
+        for i = 1 to length(data) do
+            sequence entries = split(data[i], ", ")
 
-            -- if ISTRUE
-                -- TOTAL += COUNT
+            for e = 1 to length(entries) do
+                sequence singular = split(entries[e], " ")
+                integer COL = 0
 
-        -- COUNT += 1
-        line = gets(gameNum)
+                -- set check to the colour given
+                if singular[2][1] = 'r' then
+                    COL = RED
+                elsif singular[2][1] = 'b' then
+                    COL = BLUE
+                elsif singular[2][1] = 'g' then
+                    COL = GREEN
+                end if
+
+                -- check that colour val against given 
+                if to_number(singular[1]) > COL then
+                    ISTRUE = 0
+                    printf(STDOUT, "%d - D: %d E: %d : %s\n", {u, i, e, entries[e]})
+                    exit
+                end if
+
+            end for
+            
+            -- TODO FIX THIS FU MESS
+            -- if the ID should be summed
+            if ISTRUE = 1 then
+                TOTAL += u
+                exit
+            end if
+
+        end for
     end if
+end for
 
-until find("�", line) > 0 
-end loop
-
-printf( STDOUT, "Total Real Games : %s\n", {TOTAL})
+printf(STDOUT, "Total Real Games : %d\n", {TOTAL})
 
 if gameNum = -1 then
    puts(ERROR, "No need to close the file\n")
