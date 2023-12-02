4aaae76cc57fb874e9d9fbec5c1b4c58f7b9e4c2
user <dripper1234567@gmail.com>
2023-12-02 23:16:52 +0800

    Some steps towards answering the prompt

--- Day2-Euphoria/game.ex
+++ Day2-Euphoria/game.ex
@@ -1,6 +1,8 @@
 -- https://www.tutorialspoint.com/euphoria/euphoria_files_io.htm
 -- SO HELPFULL
+include std/sequence.e
 
+-- SECTION: File Interp
 integer gameNum
 object line
 
@@ -14,12 +16,54 @@ else
    puts(STDOUT, "File opend successfully\n")
 end if
 
+-- VALUE CONST
+constant RED = 12
+constant GREEN = 13
+constant BLUE = 14
+
+-- Sum of possible games
+integer TOTAL = 0
+-- current game
+integer COUNT = 1
+
 line = gets(gameNum)
-while length(line) > 1 do
-    --TODO: FILE DATA PROCESSING
-    --printf( STDOUT, "Read content : %s\n", {line})
-    line = gets(gameNum)
-end while
+
+printf(STDOUT, "PRINT STARTS HERE")
+loop do
+    printf(STDOUT, "LOOOOOOOOOOOP")
+    sequence nocolon = split(line, ": ")
+    if length(nocolon) > 0 then
+        nocolon = nocolon[2]
+        nocolon = split(nocolon, ", ")
+        printf( STDOUT, "Total Real Games : %s\n", {nocolon[1]})
+        -- FOR THE LENGTH OF SPLIT ON SEMICOLON
+            -- ISTRUE = TRUE
+            -- FOR THE LENGTH OF SPLIT ON COMMA
+                -- SPLIT ON SPACE
+
+                -- IF 2 = red AND 1 > RED
+                    -- ISTRUE = FALSE
+                    -- EXIT
+
+                -- ELIF 2 = blue AND 1 > BLUE
+                    -- ISTRUE = FALSE
+                    -- EXIT
+
+                -- ELIF 2 = green AND 1 > GREEN
+                    -- ISTRUE = FALSE
+                    -- EXIT
+
+            -- if ISTRUE
+                -- TOTAL += COUNT
+
+        -- COUNT += 1
+        line = gets(gameNum)
+    end if
+
+until find("�", line) > 0 
+end loop
+
+printf( STDOUT, "Total Real Games : %s\n", {TOTAL})
 
 if gameNum = -1 then
    puts(ERROR, "No need to close the file\n")
