5d566e4366e8c08d0440dce9f284eb3aecffc9dc
user <dripper1234567@gmail.com>
2023-12-06 15:15:19 +0800

    Flipped from a map system to a non map system. still not working Moving on.

--- Day3-Perl/part1.pl
+++ Day3-Perl/part1.pl
@@ -2,26 +2,45 @@
 use strict;
 use warnings;
 
+#TODO: THIS. LATER. EGHH.
+
 use Scalar::Util qw(looks_like_number);
 
-
 my $file = "/home/user/Documents/AdventOfCode/Day3-Perl/values.txt";
-
 
 my $x = 0; # but her aim is getting better! But her aim is getting better!
 my $y = 0; # but her ai-
 my $total = 0;
 
+# https://www.perltutorial.org/perl-read-file/
+open(FH, '<', $file) or die $!;
+
+# the file data
+my @data = [];
+while(<FH>){
+   push(@data, split("", $_));
+}
+
+
 sub check_for_valid {
-   my($ix, $iy) = @_;
-   # TODO: check the given input for a number
-   #       Return it or return 0
+   my($ex, $ey) = @_;
+   
+   # if the number exists, return the value
+   if ($y >= 0 and $y < scalar(@data)){
+      if ($x >= 0 and $x < scalar(@data[$y])){
+         return (@data[$y][$x]);
+      }
+   }
+   
+   # if it doesn't, give 0
+   return 0;
 }
 
 sub check_moore {
    my($ix, $iy) = @_;
    my $moore = 0;
 
+   # add all the surrounding values together
    $moore += check_for_valid($ix-1, $iy-1);
    $moore += check_for_valid($ix-1, $iy);
    $moore += check_for_valid($ix-1, $iy+1);
@@ -33,29 +52,20 @@ sub check_moore {
    $moore += check_for_valid($ix+1, $iy);
    $moore += check_for_valid($ix+1, $iy+1);
 
-   if ($moore > 0){
-      return $moore;
-   }
-   
-   return 0;
+   return $moore;
 }
 
-# https://www.perltutorial.org/perl-read-file/
-open(FH, '<', $file) or die $!;
-
-# if($char ne '.' and looks_like_number($char)) 
-# if (exists $phone_of{Foo})
-
-while(<FH>){
-   foreach my $char (split("", $_)){
-    $x += 1;
+# itterate throuth the data
+foreach my @row (@data){
+   foreach my $char (@row){
     # https://www.tutorialspoint.com/how-to-check-if-a-variable-has-a-numeric-value-in-perl
-    if(looks_like_number($char)) {
-      print $x.$y;
-      # add to hash at pos ^^^
-      print "\n";
-      $total += 1;
+
+    # if it is a check around character, do that,
+    if($char ne '.' and !looks_like_number($char)){
+      $total += moore($x, $y);
     }
+
+    $x += 1;
    }
    $y += 1;
    $x = 0;
