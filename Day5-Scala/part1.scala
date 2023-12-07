2bf90c3c9128864ec0e054bf5ef9921e9f7854df
user <dripper1234567@gmail.com>
2023-12-07 14:21:27 +0800

    Base of day5, going to attempt day 6

--- Day5-Scala/part1.scala
+++ Day5-Scala/part1.scala
@@ -1,6 +1,19 @@
 import scala.io.Source 
+import scala.collection.mutable.Map
 
 @main def HelloWorld(args: String*): Unit =
   val filename = "values.txt" 
   val fileLines = Source.fromFile(filename).getLines()
   fileLines.foreach(println)
+
+  /* 
+  ________Data Format________
+  Values: ---
+
+  Map1: ---
+
+  Map2: ---
+  ...
+  ________Data Rules________
+  
+  */
